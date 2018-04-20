7729420f8c5aa89626fdb6bb87b1a951d92f1bda
nlisgo <nathan@aeopus.com>
2018-04-20 18:23:35 +0100

    Add local.settings.php

---
+++ config/local.settings.php
@@ -0,0 +1,18 @@
+<?php
+
+$databases['default']['default'] = [
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+
+$settings['trusted_host_patterns'] = [
+  '^journal\-cms\.docker\.amazee\.io$',
+];
+
+$settings['file_private_path'] = './../private';
